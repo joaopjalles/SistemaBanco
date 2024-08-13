2bf99a1607c5712ce9a597277d41b5ffd809555c
João Pedro Jalles <jpedrojalles@gmail.com>
2024-08-13 13:09:16 -0300

    att 01

---
+++ src/Desafio.java
@@ -0,0 +1,13 @@
+public class Desafio {
+    public static void main(String[] args) {
+        String nome = "João Pedro";
+        String tipoDeConta = "Corrente";
+        double saldo = 5000.00;
+
+        System.out.println("********************");
+        System.out.println("\nNome do Cliente: " + nome);
+        System.out.println("Tipo de conta: " + tipoDeConta);
+        System.out.println("Saldo da conta: " + saldo);
+        System.out.println("\n********************");
+    }
+}
